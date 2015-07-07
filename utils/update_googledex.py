499d319a36b422ccbbc05c0123c6914bc59a2592
Bradford Holden <brad.p.holden@gmail.com>
2015-07-07 13:49:28 -0700

    new update_googledex
    test script for updating the googledex after observing

---
+++ utils/update_googledex.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append("../master")
+import UCSCScheduler_V2 as ds
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print "needs a filename"
+        sys.exit()
+    fn = sys.argv[1]
+    ds.update_googledex_lastobs(fn)
