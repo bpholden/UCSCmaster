65693d008108beaa5de811459538cd02a3905cb0
Bradford Holden <brad.p.holden@gmail.com>
2015-07-31 14:29:46 -0700

    changed output columns for calc_etimes
    changed output columns for calc_etimes_precision, not longer include
    columns for meeting maximum possible time because that time is now 3
    hours

--- utils/calc_etime_precision.py
+++ utils/calc_etime_precision.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
         if star_table[i, ds.DS_APFPRI] > 9.9:
             etimes[i] = 1200.0
 
-        print "%15s %4.1f %3.1f %7.0f %7.0f %.1g %3.1f %.3g %.1f %d" % (allnames[i],star_table[i, ds.DS_APFPRI],precision[i],i2counts[i],exp_times[i],nobs[i],fin_pre[i],exp_counts[i],etimes[i],nobs[i])
+        print "%15s %4.1f %3.1f %7.0f %7.0f %.3g %.1f %d" % (allnames[i],star_table[i, ds.DS_APFPRI],precision[i],i2counts[i],exp_times[i],exp_counts[i],etimes[i],nobs[i])
 
     # plt.scatter(times, err, c=pri, cmap=plt.get_cmap("jet"), edgecolor='none')
     # plt.xlabel("Exposure Time")
