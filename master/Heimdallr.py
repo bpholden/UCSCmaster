4c1f47bf5c90af275cad49474f3bb912a8d4c993
Bradford Holden <brad.p.holden@gmail.com>
2016-07-13 16:15:09 -0700

    commented out focus calibration
    instrument focus use to be set to calibration value, that has been
    commented out as the current chiller is not handling the heat load

--- master/Heimdallr.py
+++ master/Heimdallr.py
@@ -762,12 +762,12 @@ if __name__ == '__main__':
             apflog("Cannot download googledex?! %s" % (e),level="Error")
             
         APFLib.write(apf.robot["SCRIPTOBS_LINES_DONE"], 0)
-        try:
-            APFLib.write("apfmot.DEWARFOCRAW", AVERAGE_INSTRFOC,timeout=60)
-            APFLib.write("apftask.FOCUSINSTR_LASTFOCUS", AVERAGE_INSTRFOC,timeout=60)
-            apflog("Moved instrument focus to %d" % (AVERAGE_INSTRFOC),echo=True)            
-        except:
-            apflog("Cannot move instrument focus to %d" % (AVERAGE_INSTRFOC),level="error",echo=True)
+        # try:
+        #     APFLib.write("apfmot.DEWARFOCRAW", AVERAGE_INSTRFOC,timeout=60)
+        #     APFLib.write("apftask.FOCUSINSTR_LASTFOCUS", AVERAGE_INSTRFOC,timeout=60)
+        #     apflog("Moved instrument focus to %d" % (AVERAGE_INSTRFOC),echo=True)            
+        # except:
+        #     apflog("Cannot move instrument focus to %d" % (AVERAGE_INSTRFOC),level="error",echo=True)
 
         if apf.ucam['OUTFILE'].read() == 'ucsc':
             APFTask.set(parent,suffix="LAST_OBS_UCSC", value=apf.ucam["OBSNUM"].read())
