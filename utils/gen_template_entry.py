f5eaa9185c43f239c1a048d8e5175a688d20700d
Bradford Holden <brad.p.holden@gmail.com>
2016-11-04 16:59:05 -0700

    now searches for B stars
    instead of searching by hand, the software finds the nearest B star and
    makes a star list sandwich with the B star as the bread and the
    template star as the filling

--- utils/gen_template_entry.py
+++ utils/gen_template_entry.py
@@ -8,6 +8,35 @@ from optparse import OptionParser
 from datetime import datetime
 import time
 
+def find_bstar(ras,decs,ra,dec):
+
+
+    distances = np.sqrt((ra - ras)**2 + (dec - decs)**2)
+
+    min_ind = distances.argmin()
+    
+    return min_ind
+
+def make_row(star_table,ind):
+
+    row = []
+
+    row.append(star_table[ind, ds.DS_RA])
+    row.append( star_table[ind, ds.DS_DEC])
+    row.append(star_table[ind, ds.DS_PMRA])
+    row.append(star_table[ind, ds.DS_PMDEC])
+    row.append(star_table[ind, ds.DS_VMAG])
+    row.append(1200)
+    row.append(1e9)
+    row.append( star_table[ind, ds.DS_APFPRI])
+    row.append(0)
+    if star_table[ind, ds.DS_VMAG] > 10:
+        row.append(9)
+    else:
+        row.append(5)                
+    return row
+
+
 if __name__ == "__main__":
 
     parser = OptionParser()
@@ -18,27 +47,20 @@ if __name__ == "__main__":
 
      
     allnames, star_table, do_flag, stars  = ds.parseGoogledex()
-
-   
+    bstars = np.array([ True if 'HR' in n else False for n in allnames ], dtype=bool)
+    npallnames = np.asarray(allnames)
     dt = datetime.utcfromtimestamp(int(time.time()))
     for arg in args:
         if arg in allnames:
             i = allnames.index(arg)
-            row = []
+            row = make_row(star_table,i)
 
-            row.append(star_table[i, ds.DS_RA])
-            row.append( star_table[i, ds.DS_DEC])
-            row.append(star_table[i, ds.DS_PMRA])
-            row.append(star_table[i, ds.DS_PMDEC])
-            row.append(star_table[i, ds.DS_VMAG])
-            row.append(1200)
-            row.append(1e9)
-            row.append( star_table[i, ds.DS_APFPRI])
-            row.append(0)
-            if star_table[i, ds.DS_VMAG] > 10:
-                row.append(9)
-            else:
-                row.append(5)                
+            bstari = find_bstar(star_table[:,ds.DS_RA][bstars],star_table[:,ds.DS_DEC][bstars],star_table[i,ds.DS_RA],star_table[i,ds.DS_DEC])
+            bstarrow = make_row(star_table[bstars],bstari)
+            
             line = ds.makeScriptobsLine(allnames[i],row,do_flag['do'][i],dt,decker="N",I2="N")
+            bline = ds.makeScriptobsLine(npallnames[bstars][bstari],bstarrow,'Y',dt,decker="N",I2="Y")            
+            print "%s" % (bline)
             print "%s #  %s" % (line,"pri = %s" % (star_table[i, ds.DS_APFPRI]))
+            print "%s" % (bline)            
 
