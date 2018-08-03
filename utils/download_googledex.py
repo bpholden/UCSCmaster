6a5d98e38d4ecf85c06786b45fb3dd14c6d33d38
Bradford Holden <brad.p.holden@gmail.com>
2018-08-03 16:16:52 -0700

    removed old googledex, added split

--- utils/download_googledex.py
+++ utils/download_googledex.py
@@ -3,11 +3,13 @@
 import sys
 sys.path.append("../master")
 import UCSCScheduler_V2 as ds
+import os
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print "needs a sheet list"
         sys.exit()
-    sheetns = sys.argv[1]
+    sheetns = sys.argv[1].split(",")
+    os.unlink("googledex.dat")
     ds.parseGoogledex(sheetns=sheetns)
 
