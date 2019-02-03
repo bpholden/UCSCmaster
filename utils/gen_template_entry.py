de00ec9b73db0dd19bf0918ffa83ceb6d414f8e1
Bradford Holden <brad.p.holden@gmail.com>
2019-02-03 13:42:20 -0800

    updated template entry for new refactoring

--- utils/gen_template_entry.py
+++ utils/gen_template_entry.py
@@ -1,12 +1,16 @@
 from __future__ import print_function
 import sys
 sys.path.append("../master")
-#from ExposureCalc import *
-import UCSCScheduler_V2 as ds
-import numpy as np
 from optparse import OptionParser
 from datetime import datetime
 import time
+
+import numpy as np
+
+import ParseGoogledex
+import ExposureCalculations
+import SchedulerConsts as sc
+import UCSCScheduler_V2 as ds
 
 def find_bstar(ras,decs,ra,dec):
 
@@ -21,19 +25,19 @@ def make_row(star_table,ind,bstar=False):
 
     row = []
 
-    row.append(star_table[ind, ds.DS_RA])
-    row.append( star_table[ind, ds.DS_DEC])
-    row.append(star_table[ind, ds.DS_PMRA])
-    row.append(star_table[ind, ds.DS_PMDEC])
-    row.append(star_table[ind, ds.DS_VMAG])
+    row.append(star_table[ind, sc.DS_RA])
+    row.append( star_table[ind, sc.DS_DEC])
+    row.append(star_table[ind, sc.DS_PMRA])
+    row.append(star_table[ind, sc.DS_PMDEC])
+    row.append(star_table[ind, sc.DS_VMAG])
     row.append(1200)
     row.append(1e9)
-    row.append( star_table[ind, ds.DS_APFPRI])
+    row.append( star_table[ind, sc.DS_APFPRI])
     row.append(0)
     if bstar:
         row.append(2)
     else:
-        if star_table[ind, ds.DS_VMAG] > 10:
+        if star_table[ind, sc.DS_VMAG] > 10:
             row.append(9)
         else:
             row.append(5)                
@@ -49,7 +53,7 @@ if __name__ == "__main__":
         sys.exit()
 
     fp = open("tonight","a+")
-    allnames, star_table, flags, stars  = ds.parseGoogledex()
+    allnames, star_table, flags, stars  = ParseGoogledex.parseGoogledex()
     bstars = np.array([ True if 'HR' in n else False for n in allnames ], dtype=bool)
     npallnames = np.asarray(allnames)
     dt = datetime.utcfromtimestamp(int(time.time()))
@@ -58,13 +62,13 @@ if __name__ == "__main__":
             i = allnames.index(arg)
             row = make_row(star_table,i)
 
-            bstari = find_bstar(star_table[:,ds.DS_RA][bstars],star_table[:,ds.DS_DEC][bstars],star_table[i,ds.DS_RA],star_table[i,ds.DS_DEC])
+            bstari = find_bstar(star_table[:,sc.DS_RA][bstars],star_table[:,sc.DS_DEC][bstars],star_table[i,sc.DS_RA],star_table[i,sc.DS_DEC])
             bstarrow = make_row(star_table[bstars],bstari,bstar=True)
             
             line = ds.makeScriptobsLine(allnames[i],row,flags['do'][i],dt,decker="N",I2="N",owner=flags['owner'][i])
             bline = ds.makeScriptobsLine(npallnames[bstars][bstari],bstarrow,'Y',dt,decker="N",I2="Y",owner=flags['owner'][i])            
             fp.write(bline + "\n")
-            oline ="%s #  %s\n" % (line,"pri = %s" % (star_table[i, ds.DS_APFPRI])) 
+            oline ="%s #  %s\n" % (line,"pri = %s" % (star_table[i, sc.DS_APFPRI])) 
             fp.write(oline)
             fp.write(bline + "\n")
 
