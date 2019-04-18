f4018b4cfd74ca2bdcf47dce1cab8bce5b23db35
Bradford Holden <brad.p.holden@gmail.com>
2019-04-18 10:03:14 -0700

    wrong logic!

--- master/UCSCScheduler_V2.py
+++ master/UCSCScheduler_V2.py
@@ -565,7 +565,7 @@ def getNext(ctime, seeing, slowdown, bstar=False, verbose=False,template=False,s
     moon_check = moonDist > minMoonDist
     available = available & moon_check
 
-    totobs_check = (star_table[:,DS_NOB] >= star_table[:,DS_TOT]) | (star_table[:,DS_TOT] == 0)
+    totobs_check = (star_table[:,DS_NOB] <= star_table[:,DS_TOT]) | (star_table[:,DS_TOT] == 0)
     available = available & totobs_check
     
     # We just need a B star, so restrict our math to those
