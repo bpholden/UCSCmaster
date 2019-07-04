611b1cb33c4a6da4cf9a987a57cc922fe79553a2
Bradford Holden <brad.p.holden@gmail.com>
2019-07-04 10:02:17 -0700

    added more ends

--- master/UCSCScheduler_V2.py
+++ master/UCSCScheduler_V2.py
@@ -817,9 +817,11 @@ def getNext(ctime, seeing, slowdown, bstar=False,template=False,sheetns=["Bstars
             bline = makeScriptobsLine(bname,brow,'Y',dt,decker="N",I2="Y", owner='public',focval=2)
             line  = makeScriptobsLine(sn[idx],row,'Y',dt,decker="N",I2="N", owner=flags['owner'][idx])
             bfinline = makeScriptobsLine(bnamefin,browfin,'Y',dt,decker="N",I2="Y", owner='public',focval=2)
-            res['SCRIPTOBS'] = bline + " # temp=Y\n" + line + " # temp=Y\n" + bfinline + " # temp=Y end\n"
+            res['SCRIPTOBS'] = bline + " # temp=Y\n" + line + " # temp=Y\n" + bfinline + " # temp=Y end"
             res['isTemp'] = True
             apflog("Attempting template observation of %s" % (sn[idx]),echo=True)
+    else:
+        res['SCRIPTOBS'] += "# end"
 
     return res
 
