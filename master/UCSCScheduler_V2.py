6160b1cf7ed7636500cc46a1555b151889609ad9
Bradford Holden <brad.p.holden@gmail.com>
2019-08-13 23:34:10 -0700

    crap

--- master/UCSCScheduler_V2.py
+++ master/UCSCScheduler_V2.py
@@ -596,7 +596,7 @@ def getNext(ctime, seeing, slowdown, bstar=False,template=False,sheetns=["Bstars
         else:
             ptime = datetime.utcfromtimestamp(int(time.time()))
 
-    ParseGoogledex.update_local_googledex(ptime,googledex_file=os.path.join(outdir,"googledex.dat"), observed_file=os.path.join(outdir,"observed_targets"))
+    observed = ParseGoogledex.update_local_googledex(ptime,googledex_file=os.path.join(outdir,"googledex.dat"), observed_file=os.path.join(outdir,"observed_targets"))
 
     # List of targets already observed
 
