5def65a19083d9f00700cc1352062edd017505c7
Bradford Holden <brad.p.holden@gmail.com>
2019-09-26 15:44:27 -0700

    added a function to wait for end of an exposure

--- master/TOO.py
+++ master/TOO.py
@@ -29,12 +29,26 @@ class TOO(threading.Thread):
         self.apftask = ktl.Service('apftask')
         self.scriptobs_status = self.apftask['SCRIPTOBS_STATUS']
         self.scriptobs_line = self.apftask['SCRIPTOBS_LINE']
+        
         self.scriptobs_step = self.apftask['SCRIPTOBS_STEP']
 
         self.apfucam = ktl.Service('apfucam')
         self.event = self.apfucam['EVENT']
         self.stop = self.apfucam['STOP'] # hopefully we will never need this but it will stop an exposure
+        self.exposure = self.apfucam['EXPOSURE']
+        self.elapsed = self.apfucam['ELAPSED']
+
+    def wait_end_exposure(self):
+
+        timeleft = ( self.exposure.read(binary=True) - self.elapsed.read(binary=True) ) + 40
+        rv = self.event.waitFor("== ExposureEnd",timeout=timeleft)
+        if rv:
+            rv = self.event.waitFor("== ReadoutEnd",timeout=40)
+        return rv
         
+        
+            
+
 
     def end_exposure(self):
         if self.test:
@@ -98,7 +112,11 @@ class TOO(threading.Thread):
                         if rv is False:
                             APF.log("Cannot abort scriptobs which means all kinds of bad things are happening",level='error',echo=True)
                     if len(star_table:,DS_APFPRI][next_exp]) > 0:
-                        rv self.end_scriptobs()
+                        rv = self.wait_end_exposure()
+                        if rv is False:
+                            # UCAM problem
+                            APF.log("Exposure has not ended when it is suppose to, this is a UCAM issue",level='error',echo=True)
+                        rv = self.end_scriptobs()
                         if rv is False:
                             APF.log("Cannot abort scriptobs which means all kinds of bad things are happening",level='error',echo=True)                    
             
