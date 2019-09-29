f823d34aa8b8145ee72cff967743c0d0e928e937
Bradford Holden <brad.p.holden@gmail.com>
2019-09-29 12:38:46 -0700

    adding in TOO checks

--- master/Master.py
+++ master/Master.py
@@ -56,6 +56,7 @@ class Master(threading.Thread):
         self.lastObsFinished = True
         self.fixedList = opt.fixed
         self.sheetn = opt.sheet
+        self.toosheetn = opt.toosheet
         self.targetlogname = os.path.join(os.getcwd(),"targetlog.txt")
         self.targetlog = None
         self.starttime = opt.start
@@ -167,6 +168,29 @@ class Master(threading.Thread):
         return False
 
 
+    def checkTOOs(self):
+        names, star_table, flags, stars = ParseGoogledex.parseGoogledexTOO(sheetns=self.toosheetns,outfn='too.dat',force_download=True)
+        dt = datetime.datetime.utcnow()
+        good_cadence = (ephem.julian_date(dt) - star_table[:, DS_LAST]) > star_table[:, DS_CAD]
+        immediate = (star_table[:,DS_APFPRI] > IMMEDIATE) & good_cadence
+        next_exp = (star_table[:,DS_APFPRI] < IMMEDIATE) & (star_table[:,DS_APFPRI] >NEXT_EXP) & good_cadence
+        next_line = (star_table[:,DS_APFPRI] <NEXT_EXP) & good_cadence
+                        
+        if len(star_table[:,DS_APFPRI][immediate]) > 0:
+            rv = self.endExposure()
+            rv = self.APF.killRobot(now=True)
+            if rv is False:
+                APF.log("Cannot abort scriptobs which means all kinds of bad things are happening",level='error',echo=True)
+        if len(star_table:,DS_APFPRI][next_exp]) > 0:
+            rv = self.waitEndExposure()
+            rv = self.APF.killRobot(now=True)
+            if rv is False:
+                # UCAM problem
+                APF.log("Exposure has not ended when it is suppose to, this is a UCAM issue",level='error',echo=True)
+        self.target = ds.makeTOOresult(next)
+
+
+    
     ####
     # run is the main event loop, for historical reasons it has its own functions that are local in scope
     ####
