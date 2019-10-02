35ae6b4a0f6496412db46e66b76db275b770d153
Bradford Holden <brad.p.holden@gmail.com>
2019-10-02 11:51:02 -0700

    use new helper functions

--- master/ParseGoogledex.py
+++ master/ParseGoogledex.py
@@ -208,8 +208,8 @@ def parseGoogledex(sheetns=["Bstars"],certificate='UCSC Dynamic Scheduler-5b98d1
         star_table.append(row)
         star = ephem.FixedBody()
         star.name = ls[0]
-        star._ra = ephem.hours(":".join([ls[didx["RA hr"]], ls[didx["RA min"]], ls[didx["RA sec"]]]))
-        star._dec = ephem.degrees(":".join([ls[didx["Dec deg"]], ls[didx["Dec min"]], ls[didx["Dec sec"]]]))
+        star._ra = ephem.hours(":".join(Coords.matchRACoords))
+        star._dec = ephem.degrees(":".join(Coords.matchDECCoords))
         stars.append(star)
 
     return (names, np.array(star_table), flags, stars)
