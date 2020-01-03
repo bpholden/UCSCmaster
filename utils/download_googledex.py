7ea8eb7c57f7a3df0b9873473ab13b8366999005
Bradford Holden <brad.p.holden@gmail.com>
2020-01-02 20:11:39 -0500

    now need APFTask

--- utils/download_googledex.py
+++ utils/download_googledex.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
 import sys
-sys.path.append("../master")
-import ParseGoogledex
 import os
+sys.path.append("../master")
+
+import APFTask
+import ParseGoogledex
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
@@ -11,5 +13,6 @@ if __name__ == "__main__":
     sheetns = sys.argv[1].split(",")
     if os.path.exists("googledex.dat"):
         os.unlink("googledex.dat")
+    APFTask.establish('example',os.getpid())
     ParseGoogledex.parseGoogledex(sheetns=sheetns)
 
