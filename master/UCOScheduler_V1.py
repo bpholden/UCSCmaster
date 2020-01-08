74c5cb39f5ba9508458d59575e0fd8ef2e55b1e9
Bradford Holden <brad.p.holden@gmail.com>
2020-01-08 12:25:53 -0800

    linebreak

--- master/UCOScheduler_V1.py
+++ master/UCOScheduler_V1.py
@@ -369,7 +369,8 @@ def getNext(ctime, seeing, slowdown, bstar=False,template=False,sheetns=["Bstars
         else:
             ptime = datetime.utcfromtimestamp(int(time.time()))
 
-    observed = ParseUCOSched.updateLocalGoogledex(ptime,googledex_file=os.path.join(outdir,"googledex.dat"), observed_file=os.path.join(outdir,"observed_targets"))
+    observed = ParseUCOSched.updateLocalGoogledex(ptime,googledex_file=os.path.join(outdir,"googledex.dat"),
+                                                      observed_file=os.path.join(outdir,"observed_targets"))
 
     # List of targets already observed
 
