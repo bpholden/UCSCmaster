6bf4d01b434b0baf5db7d31fba976382611b7903
Bradford Holden <brad.p.holden@gmail.com>
2020-02-26 21:02:52 -0800

    wrong order!

--- master/UCOScheduler_V1.py
+++ master/UCOScheduler_V1.py
@@ -204,14 +204,14 @@ def makeScriptobsLine(idx, star_table, t, decker="W", I2="Y", owner='public', fo
         ret += 'do= '
     # Count
     if temp:
-        count = int(star_table['APFnshots'][idx])
-    else:
         if star_table['Vmag'][idx] > 10:
             count = 9
         elif star_table['Vmag'][idx] < 8:
             count = 5
         else:
             count = 7
+    else:
+        count = int(star_table['APFnshots'][idx])
     ret += 'count=' + str(count)
             
 
