fe0d9a1a9b511f5b60c8365ee1e7404ef3b1291f
Bradford Holden <brad.p.holden@gmail.com>
2020-03-06 09:51:27 -0800

    do not loop over sheet names in updating at master level

--- master/Heimdallr.py
+++ master/Heimdallr.py
@@ -479,9 +479,8 @@ if __name__ == '__main__':
     if os.path.exists(os.path.join(os.getcwd(),"observed_targets")):
         try:
             apflog("Updating the online googledex with the observed times", level='Info', echo=True)
-            for sn in master.sheetn:
-                n = ParseUCOSched.updateSheetLastobs(os.path.join(os.getcwd(),"observed_targets"),sheetns=[sn],outfn="googledex.dat")
-                APFTask.wait(parent,True,timeout=n)
+            n = ParseUCOSched.updateSheetLastobs(os.path.join(os.getcwd(),"observed_targets"),sheetns=master.sheetn,outfn="googledex.dat")
+
 
         except Exception as e:
             apflog("Error: Updating the online googledex has failed: %s" % (e), level="error")
