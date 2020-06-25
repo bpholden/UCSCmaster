0a7e7aadd2710ea381fc9c52d991e9bd345bfb72
Bradford Holden <brad.p.holden@gmail.com>
2020-06-25 15:07:23 -0700

    back out of master obs block functionality

--- master/UCOScheduler_V1.py
+++ master/UCOScheduler_V1.py
@@ -493,13 +493,10 @@ def makeResult(stars,star_table,totexptimes,dt,idx,focval=0,bstar=False,mode='')
 
     res['obsblock'] = star_table['obsblock'][idx]
 
-    if res['obsblock'] == None or res['obsblock'].mask:
-        res['SCRIPTOBS'] = []
-        scriptobs_line = makeScriptobsLine(star_table[idx], dt, decker=res['DECKER'], owner=res['owner'], I2=star_table['I2'][idx], focval=focval)
-        scriptobs_line = scriptobs_line + " # end"
-        res['SCRIPTOBS'].append(scriptobs_line)
-    else:
-        res['SCRIPTOBS'] = makeObsBlock(star_table,idx, dt, focval)
+    res['SCRIPTOBS'] = []
+    scriptobs_line = makeScriptobsLine(star_table[idx], dt, decker=res['DECKER'], owner=res['owner'], I2=star_table['I2'][idx], focval=focval)
+    scriptobs_line = scriptobs_line + " # end"
+    res['SCRIPTOBS'].append(scriptobs_line)
 
     return res
 
