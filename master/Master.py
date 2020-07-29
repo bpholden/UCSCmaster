ed37661826b94d21db671e456cfdd2eccbac0885
Bradford Holden <brad.p.holden@gmail.com>
2020-07-29 08:49:37 -0700

    update variable for new names

--- master/Master.py
+++ master/Master.py
@@ -493,7 +493,7 @@ if __name__ == '__main__':
     if os.path.exists(os.path.join(os.getcwd(),"observed_targets")):
         try:
             apflog("Updating the online googledex with the observed times", level='Info', echo=True)
-            ParseUCOSched.updateSheetLastobs(os.path.join(os.getcwd(),"observed_targets"),sheetns=master.sheetn,outfn="googledex.dat")
+            ParseUCOSched.updateSheetLastobs(os.path.join(os.getcwd(),"observed_targets"),sheetns=opt.sheet,outfn="googledex.dat")
 
 
         except Exception as e:
