fba70a6464a7c94913d0e5069156f5ab85a1a6d4
Bradford Holden <brad.p.holden@gmail.com>
2020-08-11 11:36:48 -0700

    maybe this will work

--- master/Visible.py
+++ master/Visible.py
@@ -168,7 +168,11 @@ def is_visible_se(observer, stars, obs_len, pref_min_el=TARGET_ELEVATION_HIGH_MI
         
         diff = np.abs(s.a_dec - observer.lat)
         transit_alt = 90.0 - np.degrees(diff)
-        se = 90.0 - (transit_alt - np.degrees(s.alt))
+        se = 90.0 - (transit_alt - np.degrees(s.alt)) + offset
+
+        if cur_az > 180:
+            se -= offset
+        else:
         scaled_elevations.append(se)
         
         if fin_el < min_el or fin_el > max_el:
