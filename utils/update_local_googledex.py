4a89aa5a287a1c57963ac0bb94de1abfc5760088
Bradford Holden <brad.p.holden@gmail.com>
2020-11-22 11:57:35 -0800

    Update update_local_googledex.py
    update imports

--- utils/update_local_googledex.py
+++ utils/update_local_googledex.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env  /opt/kroot/bin/kpython
+from __future__ import print_function
 
 import sys
 import time
 import datetime
 
 sys.path.append("../master")
-import ParseGoogledex 
+import ParseUCOSched
 
 if __name__ == "__main__":
     if len(sys.argv) <= 2:
